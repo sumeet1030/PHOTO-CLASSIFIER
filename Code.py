0ea6d53832a8eb40bf6a70eeae0935d7b4239584
sumeet1030 <43828103+sumeet1030@users.noreply.github.com>
2019-08-04 20:22:05 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -7,17 +7,16 @@ from keras.applications.vgg16 import VGG16
 model = VGG16()
 # load an image from file
 image = load_img('mug.jpg', target_size=(224, 224))
-# convert the image pixels to a numpy array
 image = img_to_array(image)
 # reshape data for the model
 image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
 # prepare the image for the VGG model
 image = preprocess_input(image)
-# predict the probability across all output classes
+# predict the probability
 yhat = model.predict(image)
 # convert the probabilities to class labels
 label = decode_predictions(yhat)
-# retrieve the most likely result, e.g. highest probability
+# retrieve the most likely result
 label = label[0][0]
 # print the classification
 print('%s (%.2f%%)' % (label[1], label[2]*100))
